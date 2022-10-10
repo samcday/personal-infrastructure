dffeea38b14c9502bf22d0746c074e6076cc5a4e
Sam Day <me@samcday.com>
2022-10-10 23:09:09 +0200

    kbye

--- kube-system/cluster-autoscaler/cloud-init-generator/build-push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -uexo pipefail
-
-tag=$(cat Dockerfile update-cloud-init.sh | sha256sum | cut -d' ' -f1)
-image=samcday/cloud-init-generator:$tag
-
-docker build -t "$image" .
-docker push "$image"
